e73267001964a10325a6f0d278f1bb2abfd238f6
aerdni99 <aerdni99@gmail.com>
2025-09-25 12:51:42 -0400

    Add database connection

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.org import sessionmaker, declarative_base
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = "postgresql://{POSTGRES_USER}}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base
